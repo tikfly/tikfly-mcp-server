4d47a7a838ecb9c5c9e9c45d541dc7881970e971
bishamon82 <tikflydotio@gmail.com>
2026-04-13 11:00:26 +0700

    Add files via upload

---
+++ src/tools/search/search-accounts.ts
@@ -0,0 +1,33 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { z } from "zod";
+import { tikflyFetch } from "../../http-client.js";
+import { ok } from "../../utils/utils.js";
+
+export function registerSearchAccounts(server: McpServer) {
+  server.tool(
+    "search_accounts",
+    "Search TikTok accounts (users) by keyword. Supports pagination via cursor and search_id.",
+    {
+      keyword: z
+        .string()
+        .min(1)
+        .describe("Search term to find user accounts"),
+      cursor: z
+        .string()
+        .optional()
+        .describe("Cursor parameter is used for pagination. In the first request, the default value of cursor is 0. For subsequent requests, the value of cursor will be taken from the response of the previous request. In the Search Endpoints, both cursor and search_id need to be provided when pagination is used"),
+      search_id: z
+        .string()
+        .optional()
+        .describe("search_id parameter is used for pagination. In the first request, the default value of the search_id is 0. For subsequent requests, the value of the cursor will be taken from the response of the previous request (from: log_pb.impr_id). In the Search Endpoints, both cursor and search_id need to be provided when pagination is used"),
+    },
+    async ({ keyword, cursor, search_id }) => {
+      const data = await tikflyFetch("/api/search/account", {
+        keyword,
+        ...(cursor ? { cursor } : {}),
+        ...(search_id ? { search_id } : {}),
+      });
+      return ok(data);
+    }
+  );
+}
